4f377b5f355f92a7d721aa770bbf1500ce31d560
Phoenix <isaawong@amd.com>
2024-01-18 16:23:25 -0500

    intial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string greeting = "Hello";
+      greeting = greeting + "World";
+      Console.WriteLine("greeting: {0} !", greeting);
+    }
+  }
+}
