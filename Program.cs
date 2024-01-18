46fa9146fe559c41ebd209dbf87ec60aafddbf1c
Phoenix <isaawong@amd.com>
2024-01-18 17:14:36 -0500

    practicing writing Dictionaries, Lists, and loops

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 // Console.WriteLine("Hello, World!");
 
 using System;
+using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
@@ -9,9 +10,32 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-      string greeting = "Hello";
-      greeting = greeting + "World";
-      Console.WriteLine("greeting: {0} !", greeting);
+        // Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
+        // { "firstInning", 10 },
+        // { "secondInning", 20},
+        // { "thirdInning", 30},
+        // { "fourthInning", 40},
+        // { "fifthInning", 50}
+        // };
+
+
+        // string[] favFoods = new string[3]{ "pizza", "doughnuts", "ice cream" };
+        // string firstFood = favFoods[0];
+        // string secondFood = favFoods[1];
+        // string thirdFood = favFoods[2];
+        // Console.WriteLine("I like {0}, {1}, and {2}", firstFood, secondFood, thirdFood);
+        
+        List<string> employees = new List<string>() {
+            {"amy"},
+            {"adam"},
+        };
+
+        employees.Add("barb");
+        employees.Add("billy");
+
+        for (int i=0; i < employees.Count; i++) {
+            Console.WriteLine(employees[i]);
+        }
     }
   }
 }
