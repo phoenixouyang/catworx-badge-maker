59b2915e67040c8bab541cdbd7c1ce2c03c23ece
Phoenix <ouyangphoenix@gmail.com>
2024-01-22 21:42:55 -0500

    updates employee class to add employee ID and URL get methods

--- obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dddb1a8a386df7516b3ccdb99dd5107012cd0dbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04bfdbd48f58626c485fac827090741e3e706b04")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
