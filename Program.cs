dc0246fc2bbd580573ce724a4819ab794a411f1c
Phoenix <isaawong@amd.com>
2024-01-22 20:49:34 -0500

    creates classes for getting names and printing names

--- Program.cs
+++ Program.cs
@@ -8,20 +8,35 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
-    static void Main(string[] args)
+    static List<string> GetEmployees()
     {
-       List<string> employees = new List<string>() { "adam", "amy" };
-       employees.Add("barbara");
-       employees.Add("billy");
-       Console.WriteLine("Please enter your name");
+      List<string> employees = new List<string>();
+      while (true) 
+      {
+        Console.WriteLine("Please enter your name");
 
-       // receive name input from the console and assign it to a variable
-      string input = Console.ReadLine() ?? "";
-      employees.Add(input);
+        // receive name input from the console and assign it to a variable
+        string input = Console.ReadLine() ?? "";
+        if (input == "")
+        {
+          break;
+        }
+        employees.Add(input);
+      }
+      return employees;
+    }
+
+    static void PrintEmployees(List<string> employees)
+    {
       for (int i = 0; i < employees.Count; i++) 
       {
         Console.WriteLine(employees[i]);
       }
+    }
+    static void Main(string[] args)
+    {
+      List<string> employees = GetEmployees();
+      PrintEmployees(employees);
     }
   }
 }
--- obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46fa9146fe559c41ebd209dbf87ec60aafddbf1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+754ac8b5fa34f9e5c1d1ad859a3b8b925c92e4cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
