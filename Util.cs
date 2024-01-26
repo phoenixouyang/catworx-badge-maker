c864874ad0b2280def669675239d8b2ad9723de4
Phoenix <ouyangphoenix@gmail.com>
2024-01-25 23:03:26 -0500

    adds comments to Util

--- Util.cs
+++ Util.cs
@@ -1,3 +1,4 @@
+// require dependencies
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ namespace CatWorx.BadgeMaker
     {
         public static void PrintEmployees(List<Employee> employees)
         {
+            // run through list of entered employees and print information to terminal
             for (int i = 0; i < employees.Count; i++) 
             {
                 string template = "{0,-10}\t{1,-20}\t{2}";
@@ -20,10 +22,12 @@ namespace CatWorx.BadgeMaker
 
         public static void MakeCSV(List<Employee> employees)
         {
+            // creates data folder if it doesn't exist
             if (!Directory.Exists("data"))
             {
                 Directory.CreateDirectory("data");
             }
+            // creates CSV folder to store emtered employee data
             using (StreamWriter file = new StreamWriter("data/employees.csv"))
             {
                 file.WriteLine("ID,Name,PhotoUrl");
--- obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d7f4611faef2ad40fe6fd9b4abccdc45ac34b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b3a36ee086149f5fbc48b03357cd1c89b483415")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
