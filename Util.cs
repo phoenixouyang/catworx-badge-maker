2726c63766e041e60dfdaa36a2bad1664f5425a1
Phoenix <ouyangphoenix@gmail.com>
2024-01-25 23:13:16 -0500

    creates SKBitmap and converts to SKCanvas

--- Util.cs
+++ Util.cs
@@ -42,14 +42,23 @@ namespace CatWorx.BadgeMaker
 
         async public static Task MakeBadges(List<Employee> employees)
         {
+            int BADGE_WIDTH = 669;
+            int BADGE_HEIGHT = 1044;
+
             using (HttpClient client = new HttpClient())
             {
                 for (int i = 0; i < employees.Count; i++)
                 {
                     SKImage photo = SKImage.FromEncodedData(await client.GetStreamAsync(employees[i].GetPhotoUrl()));
+                    SKImage background = SKImage.FromEncodedData(File.OpenRead("badge.png"));
 
-                    SKData data = photo.Encode();
-                    data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+                    SKBitmap badge = new SKBitmap(BADGE_WIDTH, BADGE_HEIGHT);
+                    SKCanvas canvas = new SKCanvas(badge);
+
+                    canvas.DrawImage(background, new SKRect(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+
+                    // SKData data = background.Encode();
+                    // data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
                 }
             }
         }
